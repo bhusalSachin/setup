f1e27b975c6013d67c284a9d04fc75fcf2585748
bhusalSachin <sachinb1377@gmail.com>
2024-03-29 10:07:19 +0545

    new added

---
+++ sui-dapp-react-starter/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ConnectButton } from "@mysten/dapp-kit";
+
+function App() {
+  return (
+    <div>
+      <ConnectButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ sui-dapp-react-starter/src/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import "@mysten/dapp-kit/dist/index.css";
+
+import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
+import { getFullnodeUrl } from "@mysten/sui.js/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+const networks = {
+  devnet: { url: getFullnodeUrl("devnet") },
+  mainnet: { url: getFullnodeUrl("mainnet") },
+};
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networks} defaultNetwork="devnet">
+        <WalletProvider>
+          <App />
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
